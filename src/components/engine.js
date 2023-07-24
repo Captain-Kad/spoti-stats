8c8c66163ca986d50740226e3b3373974e02722b
Captain_Kad <buckskad@gmail.com>
2023-07-24 18:50:38 -0400

    added the code to ask user for authentication

--- src/components/engine.js
+++ src/components/engine.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Engine = () => {
   const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
@@ -7,6 +7,32 @@ const Engine = () => {
   const RESPONSE_TYPE = "token";
 
   const fullLink = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  return (
+    <div>
+      <a href={fullLink}>Click here</a>
+    </div>
+  );
 };
 
 export default Engine;
