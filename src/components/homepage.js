f8882c97123f47e1b6137ef5a16bee8297820f6d
Captain_Kad <buckskad@gmail.com>
2023-07-24 15:27:24 -0400

    created a new folder for all the components

---
+++ src/components/homepage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const HomePage = () => {
+  return (
+    <Container>
+      <Title>Spotify Stats</Title>
+    </Container>
+  );
+};
+
+export default HomePage;
+
+const Container = styled.div``;
+
+const Title = styled.h1``;
