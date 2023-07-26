254eb42d7160562036855094498846f633d5e46e
Captain_Kad <buckskad@gmail.com>
2023-07-26 13:34:49 -0400

    updated file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
-import HomePage from "./components/homepage";
+import Login from "./components/login/login";
 
 const App = () => {
   return (
     <div className="app">
-      <HomePage />
+      <Login />
     </div>
   );
 };
