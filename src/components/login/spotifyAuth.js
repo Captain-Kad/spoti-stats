e1799fc4b5b177a92d5858408de7cab95c4c9846
Captain_Kad <buckskad@gmail.com>
2023-07-26 15:43:45 -0400

    included logic for obtaining the access token

--- src/components/login/spotifyAuth.js
+++ src/components/login/spotifyAuth.js
@@ -18,6 +18,18 @@ const scopes = [
   "user-top-read",
 ];
 
+export const getToken = () => { //gets the access token from the hash
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      const parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
 export const accessUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
