3004a6181d9bf957ea973d5745e9f26268d33cc9
Captain_Kad <buckskad@gmail.com>
2023-07-26 19:26:15 -0400

    included logic to remove the access token from the url and store it so it can be used to make api requests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Login from "./components/login/login";
+import { getToken } from "./components/login/spotifyAuth";
 
 const App = () => {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    // set the token
+    const hash = getToken();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    // console.log(_token);
+  }, []);
+
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <div className="app">{token ? <h1>You are logged in</h1> : <Login />}</div>
   );
 };
 
