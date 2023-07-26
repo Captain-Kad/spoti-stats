7c4f8d83cca8ec4c02d7988a9815842922269c98
Captain_Kad <buckskad@gmail.com>
2023-07-26 13:36:29 -0400

    created a file to handle user login and gain access to the spotify api

---
+++ src/components/login/spotifyAuth.js
@@ -0,0 +1,23 @@
+export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const REDIRECT_URI = "http://localhost:3000";
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+
+const scopes = [
+  "playlist-modify-public",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "user-follow-read",
+  "user-library-read",
+  "user-modify-playback-state",
+  "user-read-playback-state",
+  "user-read-playback-position",
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-email",
+  "user-read-private",
+  "user-top-read",
+];
+
+export const accessUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
