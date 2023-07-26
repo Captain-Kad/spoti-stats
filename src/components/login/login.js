fdc74e09595ba7c170e68d5ca3fa74170d37ba6d
Captain_Kad <buckskad@gmail.com>
2023-07-26 15:41:43 -0400

    updated href for the login button

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,9 +2,7 @@ import React from "react";
 
 import { Container } from "react-bootstrap";
 import "./login.css";
-
-const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+import { accessUrl } from "./spotifyAuth";
 
 const Login = () => {
   return (
@@ -15,7 +13,7 @@ const Login = () => {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt="Spotify Logo"
       />
-      <a href={AUTH_URL} className="btn btn-success btn-lg login-button">
+      <a href={accessUrl} className="btn btn-success btn-lg login-button">
         LOGIN WITH SPOTIFY
       </a>
     </Container>
