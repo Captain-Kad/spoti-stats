7c070868b232cb03d3ecd16cbc62b1a5e7475049
Captain_Kad <buckskad@gmail.com>
2023-07-27 01:20:41 -0400

    made folder

---
+++ src/components/homepage/homepage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const HomePage = () => {
+  return (
+    <Container>
+      <Title>Spotify Stats</Title>
+    </Container>
+  );
+};
+
+export default HomePage;
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  text-align: center;
+`;
