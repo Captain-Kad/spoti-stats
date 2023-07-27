cae68a7051844b3d41b040587eeafda136df6ee2
Captain_Kad <buckskad@gmail.com>
2023-07-27 19:41:16 -0400

    updated styling

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -9,11 +9,11 @@ const HomePage = ({ spotify }) => {
   return (
     <div className="container-fluid text-center">
       <div className="row">
-        <SidebarContainer className="col-md-1">
+        <SidebarContainer className="col-md-2">
           <Sidebar />
         </SidebarContainer>
 
-        <BodyContainer className="col-md-11">
+        <BodyContainer className="col-md-10">
           <Title>Welcome To Spotify Stats</Title>
           <Header />
           {/* Body */}
@@ -27,10 +27,15 @@ export default HomePage;
 
 const SidebarContainer = styled.div`
   background-color: black;
+  height: 100vh;
+  width: 7%;
+  font-size: 16px;
 `;
 
 const BodyContainer = styled.div`
   background-color: #131516;
+  height: 100vh;
+  width: 93%;
 `;
 
 const Title = styled.h1`
