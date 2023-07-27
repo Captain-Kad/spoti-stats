e9c8b601d1f40b1b837a2b560d368acdc9881ad1
Captain_Kad <buckskad@gmail.com>
2023-07-27 18:54:04 -0400

    figured out how to use emotion styling plus bootstrap styling and moved all css styling from login css to the login js file

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,22 +1,42 @@
 import React from "react";
+import styled from "@emotion/styled";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { Container } from "react-bootstrap";
-import "./login.css";
+import logo from "../../images/spotify-login-logo.jpg";
 import { accessUrl } from "./spotifyAuth";
 
 const Login = () => {
   return (
-    <Container className="login-area">
-      <img
-        className="spotify-logo"
-        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-        alt="Spotify Logo"
-      />
-      <a href={accessUrl} className="btn btn-success btn-lg login-button">
+    <LoginContainer>
+      <LoginLogo className="spotify-logo" src={logo} alt="" />
+      <LoginButton href={accessUrl} className="btn btn-success btn-lg">
         LOGIN WITH SPOTIFY
-      </a>
-    </Container>
+      </LoginButton>
+    </LoginContainer>
   );
 };
 
 export default Login;
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const LoginLogo = styled.img`
+  margin-top: -200px;
+  margin-bottom: 100px;
+  width: 40%;
+`;
+
+const LoginButton = styled.a`
+  color: white;
+  font-weight: 700;
+  padding: 15px 30px;
+  background-color: #1db954;
+  border-radius: 99px;
+  width: 15%;
+`;
