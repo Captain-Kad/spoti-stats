d84d5bd23aa4e1f698b0f6238c6bda6b879600cc
Captain_Kad <buckskad@gmail.com>
2023-07-29 17:36:00 -0400

    updated file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,27 +14,30 @@ const spotifyApi = new SpotifyWebApi();
 const App = () => {
   const [{ user, token }, dispatch] = useDataLayerValue();
 
-  useEffect(() => {
-    // set the token
-    const hash = getToken();
-    window.location.hash = "";
-    const _token = hash.access_token;
+  useEffect(
+    () => {
+      // set the token
+      const hash = getToken();
+      window.location.hash = "";
+      const _token = hash.access_token;
 
-    if (_token) {
-      dispatch({
-        type: "SET_TOKEN",
-        token: _token,
-      });
-
-      spotifyApi.setAccessToken(_token);
-      spotifyApi.getMe().then((user) => {
+      if (_token) {
         dispatch({
-          type: "SET_USER",
-          user: user,
+          type: "SET_TOKEN",
+          token: _token,
         });
-      });
-    }
-  }, []);
+
+        spotifyApi.setAccessToken(_token);
+
+        spotifyApi.getMe().then((user) => {
+          dispatch({
+            type: "SET_USER",
+            user: user,
+          });
+        });
+      }
+    } //[]
+  );
 
   console.log(user);
 
