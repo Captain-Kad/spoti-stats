cb5076ded14093c1054c3bccdf96f176051a4bb4
Captain_Kad <buckskad@gmail.com>
2023-08-30 18:37:25 -0400

    updated code

--- src/App.js
+++ src/App.js
@@ -14,39 +14,35 @@ const App = () => {
   const [{ token, user, recently_played_tracks }, dispatch] =
     useDataLayerValue();
 
-  useEffect(
-    () => {
-      // set the token
-      const hash = getToken();
-      window.location.hash = "";
-      const _token = hash.access_token;
+  useEffect(() => {
+    // set the token
+    const hash = getToken();
+    window.location.hash = "";
+    const _token = hash.access_token;
 
-      if (_token) {
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotifyApi.setAccessToken(_token);
+
+      spotifyApi.getMe().then((user) => {
         dispatch({
-          type: "SET_TOKEN",
-          token: _token,
+          type: "SET_USER",
+          user: user,
         });
+      });
 
-        spotifyApi.setAccessToken(_token);
-
-        spotifyApi.getMe().then((user) => {
-          dispatch({
-            type: "SET_USER",
-            user: user,
-          });
+      spotifyApi.getMyRecentlyPlayedTracks().then((recently_played_tracks) => {
+        dispatch({
+          type: "SET_RECENTLY_PLAYED_TRACKS",
+          recently_played_tracks: recently_played_tracks,
         });
-
-        spotifyApi
-          .getMyRecentlyPlayedTracks()
-          .then((recently_played_tracks) => {
-            dispatch({
-              type: "SET_RECENTLY_PLAYED_TRACKS",
-              recently_played_tracks: recently_played_tracks,
-            });
-          });
-      }
-    } //[]
-  );
+      });
+    }
+  }, []);
 
   // console.log(user);
   console.log(recently_played_tracks);
