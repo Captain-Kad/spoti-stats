d9cdeec76919299778b39bbe83e78244f6cc33b5
Captain_Kad <buckskad@gmail.com>
2023-08-31 04:03:10 -0400

    changed logic so it only displays the properties of the home page

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -3,26 +3,16 @@ import styled from "@emotion/styled";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Header from "./header";
-import Sidebar from "./sidebar";
 import Logout from "./logout";
 import RecentlyPlayedTracks from "./recentlyPlayedTracks";
 
-const HomePage = ({ spotify }) => {
+const HomePage = () => {
   return (
     <HomePageContainer className="container-fluid text-center">
-      <div className="row">
-        <SidebarContainer className="col-md-2">
-          <Sidebar />
-        </SidebarContainer>
-
-        <BodyContainer className="col-md-10">
-          <Title>Welcome To Spotify Stats</Title>
-          <Header />
-          <Logout />
-          <RecentlyPlayedTracks />
-          {/* Body */}
-        </BodyContainer>
-      </div>
+      <Title>Welcome To Spotify Stats</Title>
+      <Header />
+      <Logout />
+      <RecentlyPlayedTracks />
     </HomePageContainer>
   );
 };
@@ -34,20 +24,6 @@ const HomePageContainer = styled.div`
     margin: 0;
     padding: 0;
   }
-`;
-
-const SidebarContainer = styled.div`
-  background-color: black;
-  height: 100vh;
-  width: 6.9%;
-  font-size: 14px;
-`;
-
-const BodyContainer = styled.div`
-  background-color: #131516;
-  height: 100vh;
-  width: 93.1%;
-  overflow-y: overlay;
 `;
 
 const Title = styled.h1`
