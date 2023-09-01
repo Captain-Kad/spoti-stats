868d3c10cba8e02a27cc9e1a73b68d9529a85c61
Captain_Kad <buckskad@gmail.com>
2023-09-01 02:47:57 -0400

    added the logic to show the user stored playlists followed by the styling

--- src/components/savedAlbums/library.js
+++ src/components/savedAlbums/library.js
@@ -5,10 +5,37 @@ import styled from "@emotion/styled";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Library = () => {
+  const [{ saved_playlists }] = useDataLayerValue();
+  const [userData, setUserData] = useState({
+    currentPlaylist: [],
+  });
+
+  const getUserData = async () => {
+    const userJSON = await saved_playlists;
+    setUserData({
+      currentPlaylist: userJSON.items,
+    });
+  };
+
+  getUserData();
+  console.log(userData);
+
+  const newPlaylist = userData.currentPlaylist.map((playlist) => {
+    return (
+      <Playlist className="col-md-3">
+        <div>
+          <PlaylistImage src={playlist.images[0].url} alt="" />
+          <span className="d-md-block">{playlist.name} </span>
+          <span className="d-md-block" style={{color: "rgb(169, 161, 150)"}}>{playlist.tracks.total} TRACKS</span>
+        </div>
+      </Playlist>
+    );
+  });
+
   return (
     <PageContainer>
       <h1>Your Playlists</h1>
-      <Playlists>{}</Playlists>
+      <BodyContainer className="row">{newPlaylist}</BodyContainer>
     </PageContainer>
   );
 };
@@ -17,8 +44,15 @@ export default Library;
 
 const PageContainer = styled.div``;
 
-const Playlists = styled.ul`
-  list-style-type: none;
+const BodyContainer = styled.div`
+  margin-top: 80px;
+`;
+
+const Playlist = styled.div`
+  margin-bottom: 40px;
   padding: 0;
-  margin: 0;
+`;
+
+const PlaylistImage = styled.img`
+  width: 90%;
 `;
