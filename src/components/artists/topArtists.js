894278268f66855a7e2eaaa4ef68afa6f812712b
Captain_Kad <buckskad@gmail.com>
2023-08-31 20:18:00 -0400

    created logic to handle the display of top artists

--- src/components/artists/topArtists.js
+++ src/components/artists/topArtists.js
@@ -5,11 +5,53 @@ import styled from "@emotion/styled";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const TopArtists = () => {
+  const [{ top_artists }] = useDataLayerValue();
+  const [userData, setUserData] = useState({
+    artists: [],
+  });
+
+  const getUserData = async () => {
+    const userJSON = await top_artists;
+    setUserData({
+      artists: userJSON.items,
+    });
+  };
+
+  getUserData();
+  console.log(userData);
+
+  const newArtist = userData.artists.map((artist) => {
+    return (
+      <Artist className="col-md-2">
+        <div className="fill-up">
+          <img src={artist.images[2].url} alt="" className="rounded-circle" />
+          <span className="d-md-block">{artist.name}</span>
+        </div>
+      </Artist>
+    );
+  });
+
   return (
-    <div>
+    <PageContainer>
       <h1>Top Artists</h1>
-    </div>
+      <BodyContainer className="row">{newArtist}</BodyContainer>
+    </PageContainer>
   );
 };
 
 export default TopArtists;
+
+const PageContainer = styled.div``;
+
+const BodyContainer = styled.div`
+  margin-top: 80px;
+`;
+
+const Artist = styled.div`
+  margin-bottom: 50px;
+  padding: 0;
+
+  .fill-up {
+    flex-direction: column;
+  }
+`;
